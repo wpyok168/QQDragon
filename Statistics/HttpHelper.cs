7dd7941b3407a6878f5411aafb357aec994b5dfc
NIMO <laomms@gmail.com>
2020-10-18 17:47:57 +0700

    添加项目文件。

---
+++ Statistics/HttpHelper.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    class HttpHelper
+    {
+		public static CookieCollection GetAllCookiesFromHeader(string strHeader, string strHost)
+		{
+			ArrayList al = new ArrayList();
+			CookieCollection cc = new CookieCollection();
+
+			if (!string.IsNullOrEmpty(strHeader))
+			{
+				al = ConvertCookieHeaderToArrayList(strHeader);
+				cc = ConvertCookieArraysToCookieCollection(al, strHost);
+			}
+
+			return cc;
+		}
+		private static ArrayList ConvertCookieHeaderToArrayList(string strCookHeader)
+		{
+			if (strCookHeader == null)
+			{
+				return null;
+			}
+
+			strCookHeader = strCookHeader.Replace("\r", "");
+			strCookHeader = strCookHeader.Replace("\n", "");
+			string[] strCookTemp = strCookHeader.Split(',');
+			ArrayList al = new ArrayList();
+			int i = 0;
+			int n = strCookTemp.Length;
+
+			while (i < n)
+			{
+
+				if (strCookTemp[i].IndexOf("expires=", StringComparison.OrdinalIgnoreCase) > 0)
+				{
+					al.Add(strCookTemp[i] + "," + strCookTemp[i + 1]);
+					i = i + 1;
+				}
+				else
+				{
+					al.Add(strCookTemp[i]);
+				}
+
+				i = i + 1;
+			}
+
+			return al;
+		}
+		private static CookieCollection ConvertCookieArraysToCookieCollection(ArrayList al, string strHost)
+		{
+
+			CookieCollection cc = new CookieCollection();
+			int alcount =al.Count;
+			string strEachCook = null;
+			string[] strEachCookParts = null;
+
+			for (int i = 0; i < alcount; i++)
+			{
+				strEachCook = al[i].ToString();
+				strEachCookParts = strEachCook.Split(';');
+				int intEachCookPartsCount = strEachCookParts.Length;
+				string strCNameAndCValue = string.Empty;
+				string strPNameAndPValue = string.Empty;
+				string strDNameAndDValue = string.Empty;
+				string[] NameValuePairTemp = null;
+				Cookie cookTemp = new Cookie();
+
+				for (int j = 0; j < intEachCookPartsCount; j++)
+				{
+
+					if (j == 0)
+					{
+						strCNameAndCValue = strEachCookParts[j];
+
+						if (!string.IsNullOrEmpty(strCNameAndCValue))
+						{
+							int firstEqual = strCNameAndCValue.IndexOf("=");
+							string firstName = strCNameAndCValue.Substring(0, firstEqual);
+							string allValue = strCNameAndCValue.Substring(firstEqual + 1, strCNameAndCValue.Length - (firstEqual + 1)).Trim();
+							cookTemp.Name = firstName;
+							cookTemp.Value = allValue;
+						}
+
+						continue;
+					}
+
+					if (strEachCookParts[j].IndexOf("path", StringComparison.OrdinalIgnoreCase) >= 0)
+					{
+						strPNameAndPValue = strEachCookParts[j];
+
+						if (!string.IsNullOrEmpty(strPNameAndPValue))
+						{
+							NameValuePairTemp = strPNameAndPValue.Split('=');
+
+							if (!string.IsNullOrEmpty(NameValuePairTemp[1]))
+							{
+								cookTemp.Path = NameValuePairTemp[1];
+							}
+							else
+							{
+								cookTemp.Path = "/";
+							}
+						}
+
+						continue;
+					}
+
+					if (strEachCookParts[j].IndexOf("domain", StringComparison.OrdinalIgnoreCase) >= 0)
+					{
+						strPNameAndPValue = strEachCookParts[j];
+
+						if (!string.IsNullOrEmpty(strPNameAndPValue))
+						{
+							NameValuePairTemp = strPNameAndPValue.Split('=');
+
+							if (!string.IsNullOrEmpty(NameValuePairTemp[1]))
+							{
+								cookTemp.Domain = NameValuePairTemp[1];
+							}
+							else
+							{
+								cookTemp.Domain = strHost;
+							}
+						}
+
+						continue;
+					}
+				}
+
+				if (string.IsNullOrEmpty(cookTemp.Path))
+				{
+					cookTemp.Path = "/";
+				}
+
+				if (string.IsNullOrEmpty(cookTemp.Domain))
+				{
+					cookTemp.Domain = strHost;
+				}
+
+				if (!string.IsNullOrEmpty(cookTemp.Value))
+				{
+					cc.Add(cookTemp);
+				}
+			}
+
+			return cc;
+		}
+		public static string RequestGet(string url, string headeraccept, string contentype, string referer, WebHeaderCollection heard, ref CookieContainer cookieContainers, ref string redirecturl)
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
+			ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+			{
+				return true;
+			};
+
+			//Dim domain = CStr(Regex.Match(url, "^(?:\w+://)?([^/?]*)").Groups(1).Value)
+
+			//If domain.Contains("www.") = True Then
+			//    domain = domain.Replace("www.", "")
+			//Else
+			//    domain = "." & domain
+			//End If
+			if (string.IsNullOrEmpty(url))
+			{
+				return "";
+			}
+			HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+			myRequest.Headers = heard;
+			myRequest.Method = "GET";
+			//myRequest.KeepAlive = True
+			myRequest.Accept = headeraccept;
+			myRequest.ContentType = contentype;
+			myRequest.Referer = referer;
+			myRequest.AllowAutoRedirect = false;
+			myRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36";
+			myRequest.CookieContainer = cookieContainers;
+			string results = "";
+
+			try
+			{
+				using (HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse())
+				{
+					if (myResponse.ContentEncoding.ToLower().Contains("gzip"))
+					{
+						using (Stream stream = new System.IO.Compression.GZipStream(myResponse.GetResponseStream(), System.IO.Compression.CompressionMode.Decompress))
+						{
+							using (var reader = new StreamReader(stream))
+							{
+								results = reader.ReadToEnd();
+							}
+						}
+					}
+					else if (myResponse.ContentEncoding.ToLower().Contains("deflate"))
+					{
+						using (Stream stream = new System.IO.Compression.DeflateStream(myResponse.GetResponseStream(), System.IO.Compression.CompressionMode.Decompress))
+						{
+							using (var reader = new StreamReader(stream))
+							{
+								results = reader.ReadToEnd();
+							}
+						}
+					}
+					else
+					{
+						using (Stream stream = myResponse.GetResponseStream())
+						{
+							using (var reader = new StreamReader(stream, Encoding.UTF8))
+							{
+								results = reader.ReadToEnd();
+							}
+						}
+					}
+					if (myResponse.Headers["Location"] != null)
+					{
+						redirecturl = myResponse.Headers["Location"];
+					}
+				}
+
+			}
+			catch (Exception exp)
+			{
+				Debug.WriteLine(exp.ToString());
+			}
+			redirecturl = redirecturl;
+			return results;
+		}
+		public static string RequestPost(string url, string headeraccept, string contentype, string referer, WebHeaderCollection heard, string postdata, ref CookieContainer cookieContainers, ref WebHeaderCollection myWebHeaderCollection, ref string redirecturl)
+		{
+			if (string.IsNullOrEmpty(url))
+			{
+				return "";
+			}
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
+			ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+			{
+				return true;
+			};
+
+			//Dim domain = CStr(Regex.Match(url, "^(?:\w+://)?([^/?]*)").Groups(1).Value)
+
+			//If domain.Contains("www.") = True Then
+			//    domain = domain.Replace("www.", "")
+			//Else
+			//    domain = domain
+			//End If
+			string results = "";
+			try
+			{
+
+				var myRequest = (HttpWebRequest)WebRequest.Create(url);
+				var data = Encoding.UTF8.GetBytes(postdata);
+				myRequest.Headers = heard;
+				myRequest.Method = "POST";
+				//myRequest.KeepAlive = True
+				myRequest.Accept = headeraccept;
+				myRequest.ContentType = contentype;
+				myRequest.Referer = referer;
+				myRequest.AllowAutoRedirect = false;
+				//myRequest.Headers.Add("Upgrade-Insecure-szRequests", "1")
+				myRequest.Headers.Add("Cache-Control", "max-age=0");
+				myRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36";
+				//myRequest.CookieContainer = cookieContainers
+				myRequest.Headers.Add(HttpRequestHeader.Cookie, "os=pc;osver=Microsoft-Windows-10-Professional-build-16299.125-64bit;appver=2.0.3.131777;channel=netease;__remember_me=true");
+				myRequest.ContentLength = data.Length;
+				using (var stream = myRequest.GetRequestStream())
+				{
+					stream.Write(data, 0, data.Length);
+				}
+
+				using (HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse())
+				{
+					if (myResponse.ContentEncoding.ToLower().Contains("gzip"))
+					{
+						using (var stream = myResponse.GetResponseStream())
+						{
+							using (StreamReader reader = new StreamReader(new System.IO.Compression.GZipStream(stream, System.IO.Compression.CompressionMode.Decompress), Encoding.UTF8))
+							{
+								results = reader.ReadToEnd();
+							}
+						}
+					}
+					else if (myResponse.ContentEncoding.ToLower().Contains("deflate"))
+					{
+						using (var stream = myResponse.GetResponseStream())
+						{
+							using (StreamReader reader = new StreamReader(new System.IO.Compression.DeflateStream(stream, System.IO.Compression.CompressionMode.Decompress), Encoding.UTF8))
+							{
+								results = reader.ReadToEnd();
+							}
+						}
+					}
+					else
+					{
+						using (Stream stream = myResponse.GetResponseStream())
+						{
+							using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+							{
+								results = reader.ReadToEnd();
+							}
+						}
+					}
+					if (myResponse.Headers["Location"] != null)
+					{
+						redirecturl = myResponse.Headers["Location"];
+					}
+					myWebHeaderCollection = myResponse.Headers;
+				}
+
+			}
+			catch (Exception exp)
+			{
+				Debug.WriteLine(exp.ToString());
+			}
+
+			return results;
+		}
+
+		public static string WebClientPost(string url, Dictionary<string, string> parms, WebHeaderCollection headers)
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
+			ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+			{
+				return true;
+			};
+			string result = "";
+			using (WebClient client = new WebClient())
+			{
+				try
+				{
+					client.Headers = headers;
+					client.Encoding = Encoding.UTF8;
+					var reqparm = new System.Collections.Specialized.NameValueCollection();
+					foreach (var keyPair in parms)
+					{
+						reqparm.Add(keyPair.Key, keyPair.Value);
+					}
+					byte[] responsebytes = client.UploadValues(url, "POST", reqparm);
+					result = Encoding.UTF8.GetString(responsebytes);
+				}
+				catch (WebException ex)
+				{
+					using (StreamReader r = new StreamReader(ex.Response.GetResponseStream()))
+					{
+						string responseContent = r.ReadToEnd();
+						Debug.WriteLine(responseContent);
+					}
+				}
+			}
+			return result;
+		}
+
+		public static string WebClientGet(string url, WebHeaderCollection headers, string host)
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
+			ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+			{
+				return true;
+			};
+			string result = "";
+			using (WebClient client = new WebClient())
+			{
+				try
+				{
+					client.Headers = headers;
+					client.Headers["Host"] = host;
+					//client.Headers.Add("Accept-Encoding", "gzip, deflate")
+					//client.Headers.Add("Connection", "Keep-Alive")
+					client.Encoding = Encoding.UTF8;
+					result = client.DownloadString(url);
+				}
+				catch (WebException ex)
+				{
+					using (StreamReader r = new StreamReader(ex.Response.GetResponseStream()))
+					{
+						string responseContent = r.ReadToEnd();
+						Debug.WriteLine(responseContent);
+					}
+				}
+			}
+			return result;
+		}
+
+	}
+}
---
+++ Statistics/Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Statistics
+{
+	public class Main
+	{
+
+		#region 收到私聊消息
+		public static Delegate funRecvicePrivateMsg = new RecvicePrivateMsg(RecvicetPrivateMessage);
+		[UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+		public delegate int RecvicePrivateMsg(ref PInvoke.PrivateMessageEvent sMsg);
+		public static int RecvicetPrivateMessage(ref PInvoke.PrivateMessageEvent sMsg)
+		{			
+			return 0;
+		}
+		#endregion
+		#region 收到群聊消息
+		public static RecviceGroupMsg funRecviceGroupMsg = new RecviceGroupMsg(RecvicetGroupMessage);
+		[UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+		public delegate int RecviceGroupMsg(ref PInvoke.GroupMessageEvent sMsg);
+		public static int RecvicetGroupMessage(ref PInvoke.GroupMessageEvent sMsg)
+		{
+
+			if (sMsg.SenderQQ != sMsg.ThisQQ)
+			{
+				if (sMsg.MessageContent=="谁是龙王")
+				{
+					string dragon = string.Empty;
+					IntPtr SKey = API.GetSKey(PInvoke.plugin_key, PInvoke.RobotQQ);
+					IntPtr PSKey = API.GetPSKey(PInvoke.plugin_key, PInvoke.RobotQQ, "qun.qq.com");
+					CookieContainer mycookiecontainer = new CookieContainer();
+					mycookiecontainer.Add(new Cookie("uin", PInvoke.RobotQQ.ToString()) { Domain = "qun.qq.com" });
+					mycookiecontainer.Add(new Cookie("skey", Marshal.PtrToStringAnsi(SKey)) { Domain = "qun.qq.com" });
+					mycookiecontainer.Add(new Cookie("pskey", Marshal.PtrToStringAnsi(PSKey)) { Domain = "qun.qq.com" });
+					WebHeaderCollection myWebHeaderCollection = new WebHeaderCollection();
+					var redirect_geturl = string.Empty;
+					var head1 = new WebHeaderCollection()
+		            {
+		              	{"Pragma: no-cache"},
+		            	{"DNT:1"},
+		               	{"Upgrade-Insecure-Requests:1"}
+	             	};
+					string url = "https://qun.qq.com/interactive/honorlist?gc=" + sMsg.MessageGroupQQ.ToString()+"&type=1";
+					var Res = HttpHelper.RequestGet(url, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3", "application/json, text/plain, */*", "qun.qq.com", head1,ref mycookiecontainer,ref redirect_geturl);
+					if (Res != "")
+                    {
+                        MatchCollection match1 = Regex.Matches(Res, @"\{(?:[^\{\}]|(?<o>\{)|(?<-o>\}))+(?(o)(?!))\}", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+                        foreach (Match match in match1)
+                        {
+                            try
+                            {
+								dynamic token  = new JavaScriptSerializer().DeserializeObject(match.Value);
+                                if (token["gc"] != null  && (string)token["gc"]== sMsg.MessageGroupQQ.ToString())
+                                {
+									dragon = token["talkativeList"][0]["uin"];
+								}
+                                
+                            }
+                            catch (Exception e)
+                            {
+								Debug.WriteLine(e.Message.ToString());
+                            }
+                        }
+                    }
+					if (dragon ==string.Empty)
+                    {
+						API.SendGroupMsg(PInvoke.plugin_key, sMsg.ThisQQ, sMsg.MessageGroupQQ, "[@" + sMsg.SenderQQ.ToString() + "]" + Environment.NewLine + "没有龙王.", false);
+					}
+					else
+                    {
+						API.SendGroupMsg(PInvoke.plugin_key, sMsg.ThisQQ, sMsg.MessageGroupQQ, "[@" + dragon + "]" + Environment.NewLine+ "你是本群龙王.", false);
+					}
+				}
+
+			}
+			return 0;
+		}
+		#endregion
+	}
+
+}
